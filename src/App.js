de1397f2f3f6f8c386726454f24d036ba8f7a2b7
G-RockX10 <geraldt.belton@gmail.com>
2022-11-09 22:47:54 -0500

    Initial stuff

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+import { ChatEngine } from 'react-chat-engine'
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <ChatEngine
+			height='100vh'
+			userName='Shoury'
+			userSecret='baatcheet'
+			projectID='30413a6f-5cf7-4635-ab12-748617ce371c'
+		/>
+    </div>
+  );
+}
